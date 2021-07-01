37e1dddacb4b75ce9f33ed86b1450732c1f6d786
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-07-01 16:54:43 +0100

    4_lesson: create questiont controller (test #new, #create)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,35 +1,16 @@
 class QuestionsController < ApplicationController
-  def index
-    @questions = Question.all
+  def new
+    question = Question.new
   end
-
-  def show; end
-
-  def new; end
-
-  def edit; end
 
   def create
     @question = Question.new(question_params)
 
     if @question.save
-      redirect_to @question
+      render :show
     else
       render :new
     end
-  end
-
-  def update
-    if question.update(question_params)
-      redirect_to @question
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    question.destroy
-    redirect_to questions_path
   end
 
   private
