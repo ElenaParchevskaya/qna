42aaefddbf1f0e1f678dd6f8423f86d1133744ad
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-07-05 11:03:42 +0100

    Create user authentication and answers question

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,40 +2,119 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
   let(:question) { create(:question) }
+  let(:user) { create(:user) }
+
+  before { login(user) }
+
+  describe 'GET #index' do
+    let(:questions) { create_list(:question, 3) }
+
+    before { get :index }
+
+    it 'populates an array of all questions' do
+      expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    before { get :show, params: { id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'assigns new answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+
+    context 'when question has answers' do
+      let!(:question) { create(:question, :with_answers) }
+
+      it 'has array of answers' do
+        expect(question.answers).to match_array Answer.all
+      end
+    end
+  end
 
   describe 'GET #new' do
     before { get :new }
+   it 'assigns a new Question to @question' do
+     expect(assigns(:question)).to be_a_new(Question)
+   end
 
     it 'renders new view' do
       expect(response).to render_template :new
     end
+
+    it 'has current user' do
+      expect(controller.current_user).to eq user
+    end
   end
 
   describe 'POST #create' do
-    context 'with valid attributes' do
-      subject(:request) { post :create, params: { question: attributes_for(:question) } }
+    before { login(user) }
+
+    context 'with valid attrubutes' do
+      subject(:http_request) { post :create, params: { question: attributes_for(:question) } }
 
       it 'saves a new question in the database' do
-        expect { request }.to change(Question, :count).by(1)
+        expect { http_request }.to change(Question, :count).by(1)
       end
 
-      it 'renders to show view' do
-        request
-        expect(response).to render_template :show
+      it 'redirects to show view' do
+        http_request
+        expect(response).to redirect_to assigns(:question)
       end
     end
 
-    context 'with invalid attributes' do
-      subject(:request) { post :create, params: { question: attributes_for(:question, :invalid) } }
+    context 'with invalid attrubutes' do
+      subject(:http_request) { post :create, params: { question: attributes_for(:question, :invalid) } }
 
       it 'does not save the question' do
-
-        expect { request }.not_to change(Question, :count)
+        expect { http_request }.not_to change(Question, :count)
       end
 
-      it 'renders new view' do
-        request
+      it 'renders :new view' do
+        http_request
         expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    subject(:http_request) { delete :destroy, params: { id: question } }
+
+    context 'when user is the author of question' do
+      let!(:question) { create(:question, author: user) }
+
+      it 'destroys question from the database' do
+        expect { http_request }.to change(Question, :count).by(-1)
+      end
+
+      it 'redirect to :index view' do
+        http_request
+        expect(response).to redirect_to questions_path
+      end
+    end
+
+    context 'when user is not the author of question' do
+      before { question }
+
+      it 'does not destroy question from the database' do
+        expect { http_request }.not_to change(Question, :count)
+      end
+
+      it 'renders :show view' do
+        http_request
+        expect(response).to render_template :show
       end
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of :email }
-  it { should validate_presence_of :password }
+  describe 'associations' do
+    it { is_expected.to have_many(:questions) }
+    it { is_expected.to have_many(:answers) }
+  end
+
+  describe '#author?' do
+    let(:user) { create(:user) }
+
+    context 'when return true' do
+      it 'user is author of question' do
+        question = create(:question, author: user)
+        expect(user.author?(question)).to be(true)
+      end
+
+      it 'user is author of answer' do
+        answer = create(:answer, author: user)
+        expect(user.author?(answer)).to be(true)
+      end
+    end
+
+    context 'when return false' do
+      it 'user is not author of resource' do
+        answer = create(:answer)
+        expect(user.author?(answer)).to be(false)
+      end
+    end
+  end
 end
