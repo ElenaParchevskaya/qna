954e82e8ce237218d69acba87ab0c7b85a59a72d
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-07-08 17:31:54 +0100

    Ajax

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :questions do
-    resources :answers, shallow: true
+    resources :answers, shallow: true do
+      member do
+        patch 'mark_best', as: :mark_best
+      end
+    end
   end
 
   root to: 'questions#index'
