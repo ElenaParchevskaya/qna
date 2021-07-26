78a776a751daa0c66de048dd0a9354046e809654
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-07-26 12:22:32 +0100

    add user authorization by github, google

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable, :omniauthable, omniauth_providers: %i[github  google_oauth2]
 
   has_many :questions
   has_many :answers
   has_many :awards
   has_many :comments
+  has_many :authorizations, dependent: :destroy
 
   def author_of?(resource)
     resource&.user_id == id
