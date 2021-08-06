06d58dce8dff355c38dbe3378b083c2a612ba08e
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-08-06 14:31:14 +0200

    add  subscriptions, sidekiq

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+subject(:question) { build :question }
+
   describe 'associations' do
     it { should have_many(:answers).dependent(:destroy) }
     it { should belong_to(:author).class_name('User') }
     it { should belong_to(:best_answer).class_name('Answer').optional }
     it { should have_many(:links).dependent(:destroy) }
     it { should accept_nested_attributes_for :links }
+    it { should have_many(:subscriptions).dependent(:destroy) }
   end
 
   describe 'validations' do
@@ -20,4 +23,11 @@ RSpec.describe Question, type: :model do
   it_behaves_like "linkable"
   it_behaves_like "votable"
   it_behaves_like "commentable"
+
+  describe '#subscribe_author' do
+    it "create subscription of author to question after create" do
+      question.save
+      expect(question.author).to be_subscribed(question)
+    end
+  end
 end
