cbbd0f60876047321ae96a6ae4a08e82d7acaacf
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-08-12 13:57:51 +0200

    fix .map for search_service.rb

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -1,6 +1,9 @@
 class SearchService
   def initialize(query, resources)
-    @classes = [resources].flatten.compact.map(&:classify).map(&:constantize)
+    # @classes = [resources].flatten.compact.map(&:classify).map(&:constantize)
+    @classes = [resources].flatten.compact.map do |s|
+      s.classify.constantize
+    end
     @query = ThinkingSphinx::Query.escape(query)
   end
 
